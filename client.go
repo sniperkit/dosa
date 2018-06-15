b3a8bb82cfe5925985bd013fccee2492a664569a
Xiaoxiao Fan <xiaoxiao.fan@uber.com>
2018-06-14 19:03:29 -0700

    implement CLI query subcommands (read/range)

--- client.go
+++ client.go
@@ -493,7 +493,7 @@ func (c *client) RemoveRange(ctx context.Context, r *RemoveRangeOp) error {
 	}
 
 	// now convert the client range columns to server side column conditions structure
-	columnConditions, err := convertConditions(r.conditions, re.table)
+	columnConditions, err := ConvertConditions(r.conditions, re.table)
 	if err != nil {
 		return errors.Wrap(err, "RemoveRange")
 	}
@@ -513,7 +513,7 @@ func (c *client) Range(ctx context.Context, r *RangeOp) ([]DomainObject, string,
 	}
 
 	// now convert the client range columns to server side column conditions structure
-	columnConditions, err := convertConditions(r.conditions, re.table)
+	columnConditions, err := ConvertConditions(r.conditions, re.table)
 	if err != nil {
 		return nil, "", errors.Wrap(err, "Range")
 	}
